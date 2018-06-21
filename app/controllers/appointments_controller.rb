7fdccaa8c12f261cc96c7e38c9fc99a63a6ad320
Dee Race <deerace12@gmail.com>
2018-06-21 17:47:40 +0000

    automatically backed up by learn

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,6 +1,6 @@
-class AppointmentsController < ApplicationController 
+class AppointmentsController < ApplicationController
 
-  def show 
+  def show
     @appointment = Appointment.find(params[:id])
-  end 
+  end
 end
