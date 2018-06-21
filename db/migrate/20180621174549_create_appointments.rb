082e85dc1bce7e12e7f509196e5120ded06d0bab
Dee Race <deerace12@gmail.com>
2018-06-21 18:09:53 +0000

    automatically backed up by learn

--- db/migrate/20180621174549_create_appointments.rb
+++ db/migrate/20180621174549_create_appointments.rb
@@ -1,8 +1,9 @@
 class CreateAppointments < ActiveRecord::Migration
   def change
     create_table :appointments do |t|
-
-      t.timestamps null: false
+      t.datetime :appointment_datetime
+      t.integer :patient_id
+      t.integer :doctor_id
     end
   end
 end
