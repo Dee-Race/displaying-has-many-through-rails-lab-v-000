cd9139011b380b6a43216c1a91cb61b074315eb2
Dee Race <deerace12@gmail.com>
2018-06-21 17:51:18 +0000

    automatically backed up by learn

--- app/controllers/concerns/doctors_controller.rb
+++ app/controllers/concerns/doctors_controller.rb
@@ -1,49 +1,47 @@
 class DoctorsController < ApplicationController
 
-  def show 
+  def show
     @doctor = Doctor.find(params[:id])
-  end 
+  end
 
   def index
-    @doctors = Doctor.all 
-  end 
+    @doctors = Doctor.all
+  end
 
-  def new 
-    @doctor = Doctor.new 
-  end 
+  def new
+    @doctor = Doctor.new
+  end
 
-  def edit 
+  def edit
     @doctor = Doctor.find(params[:id])
-  end 
+  end
 
-  def create 
+  def create
     @doctor.create(doctor_params)
-    if @doctor.save 
+    if @doctor.save
       redirect_to doctor_path(@doctor)
     else
-      render :new 
-    end 
-  end 
+      render :new
+    end
+  end
 
-  def update 
+  def update
     @doctor = Doctor.find(params[:id])
     if @doctor.update(doctor_params)
       redirect_to doctor_path(@doctor)
     else
-      render :edit 
-    end 
-  end 
+      render :edit
+    end
+  end
 
-  def destroy 
+  def destroy
     Doctor.find(params[:id]).destroy
-    redirect_to doctors_url 
-  end 
+    redirect_to doctors_url
+  end
 
-  private 
+  private
 
   def doctor_params
     params.require(:doctor).permit(:name, :department)
-  end 
-end 
-
-
+  end
+end
